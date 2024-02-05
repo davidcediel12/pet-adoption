be90c3b25059afa231ff9555918083f6a2a501a6
David Cediel <esdago18@gmail.com>
2024-02-05 22:43:05 +0100

    Add card component to Terms of service component

--- src/app/terms-of-service/terms-of-service.component.ts
+++ src/app/terms-of-service/terms-of-service.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CardModule} from 'primeng/card';
+import { APP_NAME } from 'src/utils/application.const';
 
 @Component({
   selector: 'app-terms-of-service',
   standalone: true,
-  imports: [],
+  imports: [CardModule],
   templateUrl: './terms-of-service.component.html',
   styleUrl: './terms-of-service.component.css'
 })
 export class TermsOfServiceComponent {
-
+  appName = APP_NAME;
 }
