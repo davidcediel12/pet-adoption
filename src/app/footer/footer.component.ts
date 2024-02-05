8bc9c358783b3777a74e7ed9bd28b8b63921b319
David Cediel <esdago18@gmail.com>
2024-02-05 12:47:31 +0100

    Create a constants file and use it in footer component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { APP_NAME } from 'src/utils/application.const';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-  sidebarVisible : boolean = false;
+  appName = APP_NAME;
 }
