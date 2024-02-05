0f778066933709390d8b1a614e547b5f4eda854b
David Cediel <esdago18@gmail.com>
2024-02-05 22:43:22 +0100

    Remove unused import in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { SidebarModule } from 'primeng/sidebar';
-import { ButtonModule } from 'primeng/button'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,10 +15,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SidebarModule,
-    ButtonModule,
-    BrowserAnimationsModule ,
-    FormsModule
+    FormsModule,    
+    BrowserAnimationsModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
