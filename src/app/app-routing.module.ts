f573a3b79a2704962b4cd1aa06d60287c6f1d1f8
David Cediel <esdago18@gmail.com>
2024-02-05 12:36:52 +0100

    Add navigation to the footer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'privacy', component: PrivacyComponent}, 
+  { path: 'terms', component: TermsOfServiceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
