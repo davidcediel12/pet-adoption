85a0111cd9c6d33142ae914f69d4ff38162abe3b
David Cediel <esdago18@gmail.com>
2024-02-16 13:16:57 +0100

    Create main page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
+  { path: '', component: MainPageComponent},
   { path : 'privacy', component: PrivacyComponent}, 
   { path: 'terms', component: TermsOfServiceComponent}
 ];
