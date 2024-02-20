0b0c6f28fe5df4e3a0d509503be68ab3a329cdae
David Cediel <esdago18@gmail.com>
2024-02-20 12:27:15 +0100

    Code main page

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [ CardModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
